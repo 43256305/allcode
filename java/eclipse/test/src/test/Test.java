26de04ada1c100584e2c53c07ac81f4c91d4312e
xiejiehui <43256305@qq.com>
2020-07-23 11:04:57 +0800

    笔记拆分前

---
+++ java/eclipse/test/src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+	public static void main(String[] args) throws Exception{
+		System.out.println("test");
+		Map<String, String> map = new HashMap<>();
+		map.put("1", "keshi");
+	}
+
+}
